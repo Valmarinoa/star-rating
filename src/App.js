f00336447ba5e312dd2c57b9d6389d4f0b88aa18
Valentina Marino <valenmarinocol@gmail.com>
2022-01-21 21:46:43 +0100

    Linked reviews state to changes in color

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 
+import React , { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
 import './App.css';
-import StarRating from './components/StarRating';
 
 function App() {
+  const [reviews, setReviews] = useState(null)
 
   return (
     <div className="App">
       <div className='stars'>
         {[...Array(5)].map((star, i) => {
-          
           const ratingKey = i + 1
 
           return <label key={i}>
-              <input type="radio" name="rating" value={ratingKey}/>
-              <StarRating />
+              <input 
+              type="radio" 
+              name="rating" 
+              value={ratingKey} 
+              onClick={() => setReviews(ratingKey)}
+              />
+              <FaStar 
+                className='eachStar' 
+                color={ratingKey <= reviews ? "#19d860" : "#e4e5e9"}
+                />
           </label>
         })}
       </div>
+      <div>
+        <h3>{reviews}</h3>
+      </div>
+
     </div>
   );
 }
